250b1cfb9d075fda3991077772aa727ad74a347e
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-03-23 18:07:57 +0530

    edited css

--- LandingPage/Script.js
+++ LandingPage/Script.js
@@ -5,16 +5,10 @@ const title2 = ["Power of AI", "Modern Finance", "into Innovation"," Anywhere, A
   function changeText() {
     let elt1 = document.getElementById("changingTitle1");
     let elt2 = document.getElementById("changingTitle2");
-    
-    // Fade out
-    elt1.style.opacity = 0;
-    elt2.style.opacity = 0;
 
     setTimeout(() => {
       elt1.textContent = title1[index]; // Change text
       elt2.textContent = title2[index];
-      elt1.style.opacity = 1; // Fade in
-      elt2.style.opacity = 1;
       index = (index + 1) % title1.length; // Loop back
     }, 500);
    
